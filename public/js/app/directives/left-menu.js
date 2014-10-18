1eaeb1eed575fdcff6babbcd9646b2c9e5753bc7
Petr Shypila <ikrumping@gmail.com>
2014-10-18 10:32:01 +0300

    Added directive for left-menu

---
+++ public/js/app/directives/left-menu.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Petr Shypila on 10/18/14.
+ */
+'use strict';
+
+angular.module('playground').directive('leftMenu', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'assets/views/left-menu.html'
+    };
+});
