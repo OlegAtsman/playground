382a120345aca63691280542142e1c20136c5cc2
Petr Shypila <ikrumping@gmail.com>
2014-10-18 10:23:45 +0300

    Added directive for google-map

---
+++ public/js/app/directives/google-map.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Petr Shypila on 10/18/14.
+ */
+'use strict';
+
+angular.module('playground').directive('googleMap', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'assets/views/map.html'
+    };
+});
