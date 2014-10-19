7783d3dc538da4e02e88f7065ee3a7e2b15cc0bd
Petr Shypila <ikrumping@gmail.com>
2014-10-19 12:32:34 +0300

    Added logo. Left menu fixed

--- public/js/app/controllers/left-menu.js
+++ public/js/app/controllers/left-menu.js
@@ -5,10 +5,9 @@
 'use strict';
 
 angular.module('playground').controller('LeftMenuCtrl', ['$scope', 'JqueryTrash', function($scope, JqueryTrash) {
-
-    $scope.menuList = [{name: 'Football'}, {name: 'Basketball'}, {name: 'Hockey'}];
-
+    $scope.menuList = [{name: 'Football'}, {name: 'Basketball', clicked: 'current'}, {name: 'Hockey'}];
     $scope.categoryClick = function(event) {
+        console.log(event);
         var target = event.target;
         JqueryTrash.categoryClick(target);
     };
