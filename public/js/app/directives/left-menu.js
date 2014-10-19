4feb05073038ae9267b657867996f2bbd74b6188
Petr Shypila <ikrumping@gmail.com>
2014-10-19 12:02:39 +0300

    Added logo. Left menu fixed

--- public/js/app/directives/left-menu.js
+++ public/js/app/directives/left-menu.js
@@ -10,10 +10,3 @@ angular.module('playground').directive('leftMenu', function() {
         controller: 'LeftMenuCtrl'
     };
 });
-
-angular.module('playground').controller('LeftMenuList', function($scope) {
-    var menuCategory1 = {categoryName: 'Sport', subcategories: ['Football', 'Basketball', 'Hockey']};
-    var menuCategory3 = {categoryName: 'Hobby', subcategories: ['Coding', 'Hacking', 'Developing']};
-    var menuCategory2 = {categoryName: 'Huyati', subcategories: ['Trash', 'Freak']};
-    $scope.menuList = [menuCategory1, menuCategory2, menuCategory3];
-});
