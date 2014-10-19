2c48620397e9e03049847fad001f2867c29fed5f
Petr Shypila <ikrumping@gmail.com>
2014-10-19 13:47:40 +0300

    login added

---
+++ public/js/app/controllers/auth.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Petr Shypila on 10/18/14.
+ */
+
+'use strict';
+
+angular.module('playground').controller('AuthCtrl', ['$scope', function($scope) {
+    var form = {};
+    $scope.login = function() {
+
+    };
+}]);
