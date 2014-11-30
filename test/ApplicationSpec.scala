45afd3e06a8bc58b60ead971d470e7fc3701aacf
Oleg Atsman <olegruno@gmail.com>
2014-11-30 23:15:34 +0300

    Update ApplicationSpec.scala

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -9,6 +9,7 @@ import play.api.test.Helpers._
  * Add your spec here.
  * You can mock out a whole application including requests, plugins etc.
  * For more information, consult the wiki.
+ * Please build it.
  */
 @RunWith(classOf[JUnitRunner])
 class ApplicationSpec extends Specification {
